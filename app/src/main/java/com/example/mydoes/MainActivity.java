8cc5a958759adb31c5c2fb23230ca4ce5cd1fac5
Aswin Ramesh <aswinramesh527@gmail.com>
2020-09-14 11:25:09 +0530

    Add Item layout fix

--- app/src/main/java/com/example/mydoes/MainActivity.java
+++ app/src/main/java/com/example/mydoes/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.mydoes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FloatingActionButton button;
+        button = findViewById(R.id.newItemBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenAddItem();
+            }
+        });
+    }
+
+    public void OpenAddItem() {
+        Intent intent = new Intent(this,AddItems.class);
+        startActivity(intent);
+
     }
 }
